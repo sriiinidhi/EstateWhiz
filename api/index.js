87bc6c6e52f1ceb42893acc3d07b35f72d7bb8c6
sriiinidhi <srinidhialukanti@gmail.com>
2025-06-21 23:37:56 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app= express();
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('Error connecting to MongoDB:', err);
+});
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
