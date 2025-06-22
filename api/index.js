e12b86ee748779dc0274326316a892038de1be66
sriiinidhi <srinidhialukanti@gmail.com>
2025-06-22 23:13:43 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,14 +1,20 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 const app= express();
+app.use(express.json());
 mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log('Connected to MongoDB');
 }).catch(err => {
   console.error('Error connecting to MongoDB:', err);
 });
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
