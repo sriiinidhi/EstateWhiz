2206d08d49f6bd312d9ebdba7ce7ae4556707670
sriiinidhi <srinidhialukanti@gmail.com>
2025-06-24 00:36:55 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -15,4 +17,32 @@ export const signup = async (req, res, next) => {
     console.error("Error during user registration:", error);
     return next(error);
   }
+};
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+            return next(errorHandler(404, "User not found"));
+        }
+        const validPassword = bcrypt.compareSync(password, validUser.password);
+        if (!validPassword) {
+            return next(errorHandler(401, "Invalid credentials"));
+        }
+            const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET );
+            const { password: _, ...userWithoutPassword } = validUser._doc; // Exclude password from the response
+            res
+            .cookie('access_token', token, {
+                httpOnly: true,
+                expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+                secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
+                sameSite: 'strict', // Prevent CSRF attacks
+            })
+            .status(200)
+            .json(userWithoutPassword);
+    } catch (error) {
+        console.error("Error during user sign-in:", error);
+        return next(error);
+    }
 };
