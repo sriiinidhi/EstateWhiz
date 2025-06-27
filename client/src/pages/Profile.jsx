0a54d9e785d89a6daa68009728422d352bc20d0d
sriiinidhi <srinidhialukanti@gmail.com>
2025-06-28 00:20:49 +0530

    add delete account

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,7 +3,7 @@ import { useRef, useState, useEffect, use} from "react";
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from "firebase/storage";
 import { app } from "../firebase"; 
 // Ensure you have firebase initialized in this file
-import { updateUserStart, updateUserSuccess, updateUserFailure } from "../redux/user/userSlice";
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 import { get, set } from "mongoose";
 
@@ -73,6 +73,23 @@ const handleSubmit = async (e) => {
     dispatch(updateUserFailure(error.message));
   }
 }
+
+const handleDeleteUser = async () => {
+  try {
+    dispatch(deleteUserStart());
+    const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+      method: "DELETE",
+    });
+    const data = await res.json();
+    if (data.success === false) {
+      dispatch(deleteUserFailure(data.message));
+      return;
+    }
+    dispatch(deleteUserSuccess(data));
+  } catch (error) {
+    dispatch(deleteUserFailure(error.message));
+  }
+}
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -126,7 +143,7 @@ const handleSubmit = async (e) => {
         </button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">
+        <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">
           Delete account
         </span>
         <span className="text-red-700 cursor-pointer"> 
